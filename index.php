d4b16c2748b8d86546efbb8febc37d2f2c927037
Alanjoose <alanjsyt@gmail.com>
2022-10-16 12:10:17 -0300

    Restart Project

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-
-require 'kernel.php';
--- kernel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Dotenv\Dotenv;
-
-require_once __DIR__.'/vendor/autoload.php';
-
-$env = Dotenv::createImmutable(__DIR__);
-$env->load();
